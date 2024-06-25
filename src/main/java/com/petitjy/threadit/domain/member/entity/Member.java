a39828e2d17bac26ef34df4db1fd59d0b396511a
given-dragon <yijy001@gmail.com>
2024-06-26 00:04:39 +0900

    #4 Feat: [BE] Member Entity Builder 추가

--- src/main/java/com/petitjy/threadit/domain/member/entity/Member.java
+++ src/main/java/com/petitjy/threadit/domain/member/entity/Member.java
@@ -2,10 +2,16 @@ package com.petitjy.threadit.domain.member.entity;
 
 import com.petitjy.threadit.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
+@Getter
 @Entity
 @Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,4 +38,15 @@ public class Member extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @ColumnDefault("'ACTIVE'")
     private MemberStatus status;
+
+    @Builder
+    private Member(Long id, String email, String password, Role role, String nickname, String profilePath, MemberStatus status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.nickname = nickname;
+        this.profilePath = profilePath;
+        this.status = status;
+    }
 }
