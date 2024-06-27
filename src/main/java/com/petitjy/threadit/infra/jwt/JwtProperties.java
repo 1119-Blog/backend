8eadd4b3f013495ae19c2d7c910dd3cf56e3cd33
given-dragon <yijy001@gmail.com>
2024-06-28 02:11:53 +0900

    #4 Refactor : [BE] expire duration 타입 변경

--- src/main/java/com/petitjy/threadit/infra/jwt/JwtProperties.java
+++ src/main/java/com/petitjy/threadit/infra/jwt/JwtProperties.java
@@ -3,18 +3,19 @@ package com.petitjy.threadit.infra.jwt;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import java.time.Duration;
 
 @Getter
 @ConfigurationProperties("jwt")
 public class JwtProperties {
-    private final String accessTokenExpireDuration;
-    private final String refreshTokenExpireDuration;
+    private final Duration accessTokenExpireDuration;
+    private final Duration refreshTokenExpireDuration;
     private final String accessTokenCookieName;
     private final String refreshTokenCookieName;
     private final String secret;
 
     @ConstructorBinding
-    public JwtProperties(String accessTokenExpireDuration, String refreshTokenExpireDuration, String accessTokenCookieName, String refreshTokenCookieName, String secret) {
+    public JwtProperties(Duration accessTokenExpireDuration, Duration refreshTokenExpireDuration, String accessTokenCookieName, String refreshTokenCookieName, String secret) {
         this.accessTokenExpireDuration = accessTokenExpireDuration;
         this.refreshTokenExpireDuration = refreshTokenExpireDuration;
         this.accessTokenCookieName = accessTokenCookieName;
