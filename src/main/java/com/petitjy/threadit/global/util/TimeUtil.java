24d93f47edfb02784cfa5ad635bfd2303bff91eb
given-dragon <yijy001@gmail.com>
2024-06-28 23:43:52 +0900

    #4 Feat : [BE] 날짜 시간 계산을 위한 TimeUtil 생성

---
+++ src/main/java/com/petitjy/threadit/global/util/TimeUtil.java
@@ -0,0 +1,19 @@
+package com.petitjy.threadit.global.util;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class TimeUtil {
+    public static Date localDateTime2Date(LocalDateTime localDateTime) {
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        return Date.from(instant);
+    }
+
+    public static LocalDateTime date2LocalDateTime(Date date) {
+        return date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+}
