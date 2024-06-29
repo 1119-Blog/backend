46133ebdcf20e5b18d0a5ee05d70f581d9da6dcf
Hyunuk <dksgusdnr17@naver.com>
2024-06-30 06:19:42 +0900

    #13 Feat: [BE] Member DTO 클래스 추가

---
+++ src/main/java/com/petitjy/threadit/domain/member/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.petitjy.threadit.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MemberDto {
+	private Long id;
+}
