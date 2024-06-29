ece064c0978f9620958efeb95779b6707477a27a
given-dragon <yijy001@gmail.com>
2024-06-30 01:55:51 +0900

    #4 Feat : [BE] oauth 로그인 성공 시 token 발급 추가

--- src/main/java/com/petitjy/threadit/domain/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/petitjy/threadit/domain/oauth/handler/OAuth2SuccessHandler.java
@@ -1,5 +1,9 @@
 package com.petitjy.threadit.domain.oauth.handler;
 
+import com.petitjy.threadit.domain.member.entity.Member;
+import com.petitjy.threadit.domain.oauth.dto.MemberPrincipalDetails;
+import com.petitjy.threadit.infra.jwt.JwtProperties;
+import com.petitjy.threadit.infra.jwt.JwtProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,9 +12,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -18,17 +24,22 @@ class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Value("${oauth.base-uri}")
     private String baseUri;
 
+    private final JwtProvider jwtProvider;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setHeader("Content-Type", "application/json");
 
-        ResponseCookie accessToken = ResponseCookie.from("access_token", "temp-token1")
+        String accessToken = jwtProvider.createToken(authentication, jwtProvider.getAccessTokenExpirationDate());
+        String refreshToken = jwtProvider.createToken(authentication, jwtProvider.getRefreshTokenExpirationDate());
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from("access_token", accessToken)
                 .path("/").httpOnly(true).build();
-        ResponseCookie refreshToken = ResponseCookie.from("refresh_token", "temp-token2")
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", refreshToken)
                 .path("/").httpOnly(true).build();
 
-        response.addHeader(HttpHeaders.SET_COOKIE, accessToken.toString());
-        response.addHeader(HttpHeaders.SET_COOKIE, refreshToken.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
         response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
         response.setHeader("Location", baseUri + "/");
