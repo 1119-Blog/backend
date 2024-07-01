bc1fa564b25c54c7be7b2117974d1096244b85df
given-dragon <yijy001@gmail.com>
2024-07-01 18:06:01 +0900

    #4 Refactor : [BE] Member Status & Role 불필요한 코드 삭제

--- src/main/java/com/petitjy/threadit/domain/member/entity/MemberStatus.java
+++ src/main/java/com/petitjy/threadit/domain/member/entity/MemberStatus.java
@@ -1,17 +1,7 @@
 package com.petitjy.threadit.domain.member.entity;
 
 public enum MemberStatus {
-    ACTIVE("ACTIVE"),
-    SUSPEND("SUSPEND"),
-    DELETED("DELETED");
-
-    final String status;
-
-    MemberStatus(String status) {
-        this.status = status;
-    }
-
-    public String value() {
-        return status;
-    }
+    ACTIVE,
+    SUSPEND,
+    DELETED
 }
--- src/main/java/com/petitjy/threadit/domain/member/entity/Role.java
+++ src/main/java/com/petitjy/threadit/domain/member/entity/Role.java
@@ -1,17 +1,7 @@
 package com.petitjy.threadit.domain.member.entity;
 
 public enum Role {
-    MEMBER("MEMBER"),
-    ANONYMOUS("ANONYMOUS"),
-    ADMIN("ADMIN");
-
-    private final String role;
-
-    Role(String role) {
-        this.role = role;
-    }
-
-    public String value() {
-        return role;
-    }
+    MEMBER,
+    ANONYMOUS,
+    ADMIN
 }
