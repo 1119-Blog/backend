794fe7f768308073014b1e0d9e1de7961670fd76
given-dragon <yijy001@gmail.com>
2024-07-01 17:52:47 +0900

    #4 Refactor : [BE] base uri 불변 추가, header name 상수로 변경

--- src/main/java/com/petitjy/threadit/domain/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/petitjy/threadit/domain/oauth/handler/OAuth2SuccessHandler.java
@@ -1,34 +1,31 @@
 package com.petitjy.threadit.domain.oauth.handler;
 
-import com.petitjy.threadit.domain.member.entity.Member;
-import com.petitjy.threadit.domain.oauth.dto.MemberPrincipalDetails;
-import com.petitjy.threadit.infra.jwt.JwtProperties;
 import com.petitjy.threadit.infra.jwt.JwtProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Component
 class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
-    @Value("${oauth.base-uri}")
-    private String baseUri;
 
+    private final String BASE_URI;
     private final JwtProvider jwtProvider;
+
+    public OAuth2SuccessHandler(@Value("${oauth.base-uri}") String baseUri, JwtProvider jwtProvider) {
+        this.BASE_URI = baseUri;
+        this.jwtProvider = jwtProvider;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.setHeader("Content-Type", "application/json");
+        response.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
 
         String accessToken = jwtProvider.createToken(authentication, jwtProvider.getAccessTokenExpirationDate());
         String refreshToken = jwtProvider.createToken(authentication, jwtProvider.getRefreshTokenExpirationDate());
@@ -37,11 +34,12 @@ class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                 .path("/").httpOnly(true).build();
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", refreshToken)
                 .path("/").httpOnly(true).build();
+        // TODO Refresh token 저장
 
         response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
         response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
+        response.addHeader(HttpHeaders.LOCATION, BASE_URI);
         response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-        response.setHeader("Location", baseUri + "/");
     }
 }
